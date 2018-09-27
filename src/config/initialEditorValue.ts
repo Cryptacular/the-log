fe75d79b1c4a3e40d5e4b6dd55ffc7e1e81fd85a
Nick Mertens <nick.mertens@fphcare.co.nz>
2018-09-27 13:13:24 +1200

    Set initial values to be set to the first day of opening the app.

--- src/config/initialEditorValue.ts
+++ src/config/initialEditorValue.ts
@@ -1,29 +1,23 @@
-import { LogItem } from "../models/LogItem";
-import { LogType } from "../models/LogType";
-import { Tag } from "../models/Tag";
+import { LogItem } from '../models/LogItem';
+import { LogType } from '../models/LogType';
+import { Tag } from '../models/Tag';
 
-const tutorialTag = new Tag(1, "tutorial");
+const tutorialTag = new Tag(1, 'tutorial');
 
 const logs: LogItem[] = [
-  new LogItem(
-    LogType.Note,
-    new Date(2018, 9, 22),
-    "This is what a note looks like",
-    [tutorialTag]
-  ),
-  new LogItem(
-    LogType.Event,
-    new Date(2018, 9, 22),
-    "Whereas this is an event",
-    [tutorialTag]
-  ),
-  new LogItem(LogType.Task, new Date(2018, 9, 22), "Add your first task here", [
+  new LogItem(LogType.Note, new Date(), 'This is what a note looks like', [
+    tutorialTag
+  ]),
+  new LogItem(LogType.Event, new Date(), 'Whereas this is an event', [
+    tutorialTag
+  ]),
+  new LogItem(LogType.Task, new Date(), 'Add your first task here', [
     tutorialTag
   ]),
   new LogItem(
     LogType.Done,
-    new Date(2018, 9, 22),
-    "Then press Ctrl/Cmd+D to complete the task, like this one!",
+    new Date(),
+    'Then press Ctrl/Cmd+D to complete the task, like this one!',
     [tutorialTag]
   )
 ];
